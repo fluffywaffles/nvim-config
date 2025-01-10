8bb296d8c97783a582c14c76de6f9a5f1d0c12ca
fluffywaffles <skorlir@gmail.com>
2025-01-10 12:08:41 -0800

    lsp: set up elixir-tools

--- lua/init.lua
+++ lua/init.lua
@@ -45,6 +45,11 @@ paq:setup(paq_config) {
   'nathanaelkane/vim-indent-guides',
   -- kitty-scrollback for opening the kitty scrollback buffer in neovim
   'mikesmithgh/kitty-scrollback.nvim',
+  -- plenary.nvim, a standard library of sorts
+  'nvim-lua/plenary.nvim',
+  -- elixir-tools
+  -- NOTE: depends on plenary.nvim
+  'elixir-tools/elixir-tools.nvim',
 }
 
 -- general editor configuration
@@ -166,41 +171,30 @@ lsp.lua_ls.setup(coq.lsp_ensure_capabilities{
 })
 
 -- elixirls
-function StartElixirLS()
-  -- find the binary installed from the AUR package elixir-ls
-  local lang_server = vim.fn.systemlist('which elixir-ls')[1]
-  -- if the binary does not exist, error out
-  if string.match(lang_server, '.*not found') ~= nil then
-    print('cannot find elixir-ls server in path')
-    return
-  end
-  -- find all mix project configuration files up to $HOME
-  local mix_hierarchy = vim.fs.find('mix.exs', {
-    upward = true,
-    stop = vim.loop.os_homedir(),
-    limit = math.huge, -- no effective limit
-  })
-  -- start the language server
-  vim.lsp.start(coq.lsp_ensure_capabilities({
-    name = 'elixirls',
-    cmd_env = { SHELL = "bash" },
-    cmd = { lang_server },
-    -- assume the topmost mix.esx file is the root of the project
-    --   ** use topmost assuming we may be in an umbrella app
-    root_dir = vim.fs.dirname(mix_hierarchy[1]),
-  }))
-end
-
--- start elixir-ls via autocmd
-local au_elixir = vim.api.nvim_create_augroup('elixir', {})
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-  group = au_elixir,
-  pattern = { 'elixir', 'heex' },
-  callback = function()
-    -- start the language server
-    StartElixirLS()
-  end
-})
+require('elixir').setup {
+  nextls = {
+    enable = true,
+    init_options = {
+      experimental = {
+        completions = {
+          enable = true,
+        },
+      },
+    },
+  },
+  elixirls = {
+    enable = true,
+    settings = require('elixir.elixirls').settings {
+      dialyzerEnabled = true,
+      enableTestLenses = false,
+    },
+    on_attach = function(_ --[[client]], _ --[[bufnr]])
+      vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+      vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+      vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+    end,
+  },
+}
 
 -- typescript tsserver
 function StartTsserver()

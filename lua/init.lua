0ccab3d8f739ea28b3d170a54241979a5eb45c55
fluffywaffles <skorlir@gmail.com>
2026-09-09 12:25:28 -0700

    plugins: vimade: fade unfocused vim windows or terminal panes containing

--- lua/init.lua
+++ lua/init.lua
@@ -180,6 +180,16 @@ require('vimade').setup({
   recipe = { "default", { animate = true } },
   enablefocusfading = true,
   fadelevel = 0.4,
+  blocklist = {
+    -- ignore status lines and bars between vim windows / splits
+    preserve_separators = {
+      highlights = {
+        'StatusLineNC',
+        'WinSeparator',
+        'VertSplit',
+      },
+    },
+  },
 })
 
 --[[
